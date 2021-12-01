88d154cb4e5920fe899b0cb351223511acf9a36e
愛梨 太田 <190703@jc-21.jp>
2021-12-01 17:33:46 +0900

    まとめ２

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -67,16 +67,42 @@ namespace R03BMI
                 
                 double BMI = weight2 / (height2 * height2);
                 double BMI2 = Math.Round(BMI,1);
-                //string BMI3 = BMI2.ToString();
+                string status = null;
                 
-               
-                result.Text = "BMI値は"+ BMI2 +"です。";
-                Console.WriteLine(BMI2);
+                if (BMI2 < 18.5)
+                {
+                    status = "低体重 (痩せ)	";
+                }
+
+                else if (BMI2 < 25)
+                {
+                    status = "普通体重";
+                }
+
+                else if (BMI2 < 30)
+                {
+                    status = "肥満 (1度)";
+                }
+
+                else if (BMI2 < 35)
+                {
+                    status = "肥満 (2度)";
+                }
+
+                else if (BMI2 < 40)
+                {
+                    status = "肥満 (3度)";
+                }
                 
-                //result.Text=BMI2.ToString();
-                
-               
-                //result.Text=BMI2;
+                else
+                {
+                    status = "肥満 (4度)";
+                }
+
+                result.Text = "BMI値は"+BMI2+"状態は"+status+"です。";
+              
+
+
               
             }
             catch(FormatException ex)
